b840590f73084adc0b1116ebebe6c3cdb41e6ba1
Mahshid <mahshid@joblookup.com>
2026-05-25 22:22:03 +0100

    Add More artworks

--- app/composables/useProjects.ts
+++ app/composables/useProjects.ts
@@ -53,7 +53,7 @@ export const useProjects = () => {
 			fullDesc: 'I made this illustration using the Krita app on a Samsung tablet. I started with a rough sketch, using a photo as a reference to get the facial features right. I then added details and shading, focusing on texture and bold lines. The background text adds extra character to the drawing. This was a fun practice in portrait art and trying out different brushes in Krita.',
 			cover: '/art/5.jpg',
 			gallery: [
-				'/art/5.jpg',
+				'/art/5/5.jpg',
 				'/art/5/1.jpg',
 				'/art/5/2.jpg',
 				'/art/5/3.jpg',
@@ -78,7 +78,7 @@ export const useProjects = () => {
 			fullDesc: '',
 			cover: '/art/7.jpg',
 			gallery: [
-				'/art/7.jpg',
+				'/art/7/1.jpg',
 			]
 		},
 		{
@@ -94,6 +94,94 @@ export const useProjects = () => {
 				'/art/8/2.jpg',
 				'/art/8/4.jpg',
 				'/art/8/5.jpg',
+			]
+		},
+		{
+			id: '10',
+			title: 'Procreate Practice',
+			shortDesc: '',
+			fullDesc: '',
+			cover: '/art/10.jpg',
+			gallery: [
+				'/art/10.jpg',
+			]
+		},
+		{
+			id: '9',
+			title: 'Color and Texture Study: Realistic Pumpkin',
+			shortDesc: '',
+			fullDesc: 'A digital study focused on realistic textures and colors. Created on a Samsung tablet with Krita, this piece uses precise layering and color swatches to capture the warm details of a pumpkin.',
+			cover: '/art/9.jpg',
+			gallery: [
+				'/art/9.jpg',
+				'/art/9/1.jpg',
+				'/art/9/2.jpg',
+			]
+		},
+		{
+			id: '11',
+			title: 'My Sister!',
+			shortDesc: '',
+			fullDesc: '',
+			cover: '/art/11.jpg',
+			gallery: [
+				'/art/11.jpg',
+				'/art/11/2.jpg',
+				'/art/11/1.jpg',
+				'/art/11/3.jpg',
+			]
+		},
+		{
+			id: '12',
+			title: 'Me!',
+			shortDesc: '',
+			fullDesc: '',
+			cover: '/art/12.jpg',
+			gallery: [
+				'/art/12/2.jpg',
+				'/art/12/1.jpg',
+			]
+		},
+		{
+			id: '13',
+			title: 'Monster',
+			shortDesc: '',
+			fullDesc: '',
+			cover: '/art/13.png',
+			gallery: [
+				'/art/13.png',
+				'/art/13/1.jpg',
+			]
+		},
+		{
+			id: '14',
+			title: 'Chips',
+			shortDesc: '',
+			fullDesc: '',
+			cover: '/art/14.jpg',
+			gallery: [
+				'/art/14.jpg',
+			]
+		},
+		{
+			id: '15',
+			title: 'Chiaroscuro Drawing',
+			shortDesc: '',
+			fullDesc: '',
+			cover: '/art/15.jpg',
+			gallery: [
+				'/art/15.jpg',
+				'/art/15/1.jpg',
+			]
+		},
+		{
+			id: '16',
+			title: 'Sketches',
+			shortDesc: '',
+			fullDesc: '',
+			cover: '/art/16.jpg',
+			gallery: [
+				'/art/16.jpg',
 			]
 		},
 	]
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -2,7 +2,7 @@
 	<section>
 		<div class="text-center bg-pink-ink text-white p-12 rounded-[15px_50px_15px_50px] mt-8 shadow-lg">
 			<h2 class="font-handwriting text-5xl mb-4 text-white">Let's Work Together!</h2>
-			<p class="text-lg mb-6">For commissions, editorial work, or collaborations, please reach out via email:</p>
+			<p class="text-lg mb-6">For commissions, editorial work, or collaborations, please reach out on Instagram:</p>
 			<p class="mt-8 text-lg">
 				Instagram: <a href="https://www.instagram.com/mahdis_illustrator" target="_blank" class="font-bold hover:underline">@mahdis_illustrator</a>
 			</p>
